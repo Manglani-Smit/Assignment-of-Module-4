5683dda71a1b4b6956fded02139af243bca75468
Smit <sangitamanglani0812@gmail.com>
2026-09-20 09:40:51 +0530

    Session 8 Completed

--- Session 8/newapp/views.py
+++ Session 8/newapp/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        try:
+            user=User.objects.get(email=request.POST['email'])
+            msg="Email already exists"
+            return render(request,'signup.html',{'msg':msg})
+        except:
+            if request.POST['password'] == request.POST['cpassword']:
+                User.objects.create(
+                    fname=request.POST['fname'],
+                    email=request.POST['email'],
+                    password=request.POST['password'],
+                )
+                msg="Signup successful"
+                return render(request,'signup.html',{'msg':msg})
+            else:
+                msg="Passwords do not match"
+                return render(request,'signup.html',{'msg':msg})
+    else:
+        return render(request,'signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        try:
+            user=User.objects.get(email=request.POST['email'])
+            if user.password == request.POST['password']:
+                request.session['email']=user.email
+                request.session['username']=user.fname
+                msg="Login successful"
+                return render(request,'index.html',{'msg':msg})
+            else:
+                msg="Invalid email or password"
+                return render(request,'login.html',{'msg':msg})
+        except:
+            msg="Email does not exist"
+            return render(request,'login.html',{'msg':msg})
+    else:
+        return render(request,'login.html')
+
+def logout(request):
+        try:
+            del request.session['email']
+            del request.session['username']
+            return render(request,'login.html')
+        except:
+            return render(request,'login.html')
--- Session 8/newproject/urls.py
+++ Session 8/newproject/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from newapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",views.index),
+    path("signup/",views.signup,name="signup"),
+    path("login/",views.login,name="login"),
+    path("logout/",views.logout,name="logout"),
+
 ]
